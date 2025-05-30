92a71992a35580daeb17ec542ad87b53e55e3b97
Johan Vela <johan.vela@unas.edu.pe>
2025-05-30 18:27:00 -0500

    Creación de la interfaz del componente login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  usuario = '';
+  password = '';
+  error: string | null = null;
 
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    if (this.usuario === 'admin' && this.password === '1234') {
+      // Autenticación exitosa (esto es un ejemplo)
+      this.router.navigate(['/usuarios']); // o la ruta que quieras mostrar después
+    } else {
+      this.error = 'Usuario o contraseña incorrectos';
+    }
+  }
+
+    navigateToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
